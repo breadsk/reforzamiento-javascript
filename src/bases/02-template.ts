584b1a9104a2f9f5dde92ef11e925bf356c6a36f
Nicolas <nicolas.programador@gmail.com>
2025-09-16 19:59:05 -0300

    Template Strings

---
+++ src/bases/02-template.ts
@@ -0,0 +1,13 @@
+// /n salto de linea
+
+
+const firstName = 'Nicolas'
+const lastName = "Caceres"
+
+console.log(firstName , lastName);
+
+//En el signo dolar se agrega una expresion de javascript
+const fullName = `${firstName} ${lastName}`
+
+console.log(fullName);
+
