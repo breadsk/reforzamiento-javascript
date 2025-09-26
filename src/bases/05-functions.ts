f59c096867b2c352536292805760982a1718c001
Nicolas <nicolas.programador@gmail.com>
2025-09-25 23:14:36 -0300

    Funciones

--- src/bases/05-functions.ts
+++ src/bases/05-functions.ts
@@ -1,5 +1,53 @@
 
-function greet(name:string){
+function greet(name:string):string{
     return `Hola ${ name }`
 }
+
+
+const greet2 = (name:string) => {
+    return `Hola ${ name }`
+}
+
+//En cambipo esto te arroja el error de inmediato
+// greet2 = () => {
+//     console.log("Hola");
+// }
+
+
+//Con esto solo me arroja una advertencia en ts
+// greet = function(){
+//     console.log("Hola");
+// }
+
+// greet()
+
+//
+
+const message = greet('Nicolas');
+const message2 = greet2('Vladimir');
+
+
+console.log(message);
+console.log(message2);
+
+interface userInterface {
+    uid:string;
+    username:string;
+}
+
+
+function getUser(){
+    return {
+        uid: 'ABC-123',
+        username: 'zenakulee'
+    }
+}
+
+const getUser2 = ():userInterface => ({
+    uid: 'ABC-123',
+    username: 'zenakulee'
+});
+
+const user = getUser()
+console.log(user);
 
