e16fc432c32091f97c8306db2987478335bd373d
Nicolas <nicolas.programador@gmail.com>
2025-10-02 19:08:33 -0300

    Ejemplo funciones

--- src/bases/05-functions.ts
+++ src/bases/05-functions.ts
@@ -4,9 +4,8 @@ function greet(name:string):string{
 }
 
 
-const greet2 = (name:string) => {
-    return `Hola ${ name }`
-}
+const greet2 = (name:string) => `Hola ${name}`;
+
 
 //En cambipo esto te arroja el error de inmediato
 // greet2 = () => {
@@ -35,19 +34,36 @@ interface userInterface {
     username:string;
 }
 
-
-function getUser(){
-    return {
-        uid: 'ABC-123',
-        username: 'zenakulee'
-    }
-}
-
-const getUser2 = ():userInterface => ({
+//Con esto tengo un return implicito
+const getUser = ():userInterface => ({
     uid: 'ABC-123',
-    username: 'zenakulee'
+    username: 'zenakulee',
+})
+
+
+// function getUser(){
+//     return {
+//         uid: 'ABC-123',
+//         username: 'zenakulee'
+//     }
+// }
+
+// const getUser2 = ():userInterface => ({
+//     uid: 'ABC-123',
+//     username: 'zenakulee'
+// });
+
+// const user = getUser()
+// console.log(user);
+
+
+
+const myNumbers: number[] = [1,2,3,4,5];
+
+// myNumbers.forEach((value)=> {
+//     console.log(value);
+// })
+//myNumbers.forEach(console.log)
+myNumbers.forEach((value,index,arr) => {
+    console.log(value + " " + index + " " + arr);    
 });
-
-const user = getUser()
-console.log(user);
-
